ca2c0ab358ec061e4bad5e00b0f4eafbe916a284
Katharina Köhler <kathi_koehler@web.de>
2016-04-05 00:59:15 +0200

    corrects the cvs file with no errors left

--- homework/readAndCorrect.js
+++ homework/readAndCorrect.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var data = '';
-var fileName = "/Users/kathi/Downloads/Liste_PPN-ExNr_HSHN-libre.csv";
+var fileName = "/Users/kathi/Datenanalyse/homework/test.csv";
 
 // read contents of file
 var contents = fs.readFileSync(fileName).toString();
@@ -14,18 +14,25 @@ var array = [];
 // first line contains description of columns
 var firstLine = lines.shift().split(",");
 
-// output stream for broken lines ...
+// output stream for broken lines
 var brokenStream = fs.createWriteStream('errors.txt');
 
-// output for dump of resulting array
+// output for resulting array
 var resultStream = fs.createWriteStream('results.txt');
+
+// output stream for statistic analysis
+var statisticStream = fs.createWriteStream('statistic.txt');
+
 
 if (firstLine.length < 5) {
     process.exit();
 }
 
+var tokens;
+
 lines.forEach(function(line) {
-    var tokens = line.split(",");
+    //split lines at comma but ignore comma between double quotes
+    tokens = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
 
     // in case csv is NOT wellformed in all lines .. write broken lines to output file
     if (tokens.length != firstLine.length) {
@@ -49,11 +56,18 @@ lines.forEach(function(line) {
     }
 
     array.push(dataObject);
+
 });
+
 
 var util = require('util');
 
 resultStream.write(util.inspect(array, false, null));
-console.log("extracted " + array.length + " objects from csv.");
+resultStream.close();
 
+console.log("extracted " + array.length + " objects from csv.");
 console.log("Program ended.");
+
+
+
+
